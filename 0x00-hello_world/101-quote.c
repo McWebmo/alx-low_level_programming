343e94faf8dad74d21f293b39923bbae89e792e8
McWebmo <mcwebmo@gmail.com>
2023-08-10 02:29:42 -0700

    add 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program prints a quote without using printf or puts
+ * Return: always 1 (Success)
+ */
+
+int main(void)
+{
+	char quote[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,quote,61)
+	return (1);
+}
