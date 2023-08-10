819e85b898d9d35b7aa9467659def936f2d9a768
McWebmo <mcwebmo@gmail.com>
2023-08-10 01:37:54 -0700

    add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: the program prints a string using puts function from stdio.h
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
