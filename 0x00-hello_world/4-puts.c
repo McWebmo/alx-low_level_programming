3e2778e6cf30c6f315638edb403a105590caab05
McWebmo <mcwebmo@gmail.com>
2023-08-10 01:35:27 -0700

    fix 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: the program prints a string using puts function from stdio.h
+ *
+ * Return: always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
