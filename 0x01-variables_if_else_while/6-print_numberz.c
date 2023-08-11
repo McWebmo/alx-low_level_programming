3c2b746bdcaac01157f4f0bbe324fb92b2057f2a
McWebmo <mcwebmo@gmail.com>
2023-08-11 16:01:57 -0700

    add 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	printf("\n");
+	return (0);
+}
