0f524a351a7bb45183e197a9f12d47fe2f1cc1ae
McWebmo <mcwebmo@gmail.com>
2023-08-11 16:00:27 -0700

    add 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	return (0);
+}
