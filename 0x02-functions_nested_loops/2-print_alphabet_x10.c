a6a0b239adaa124c5892f9c7438257acc6a478b4
McWebmo <mcwebmo@gmail.com>
2023-08-15 04:20:41 -0700

    add 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Description: prints the alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void);
+{
+	int i = 0;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			char c = i;
+
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+
+
+
+}
