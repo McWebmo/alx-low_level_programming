1a562aaacd6f35f4c89f60f5398b97947e6ee61f
McWebmo <mcwebmo@gmail.com>
2023-08-15 14:46:33 -0700

    fix 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,27 @@
  * Return: 0
  */
 
-int main(void)
+void jack_bauer(void);
 {
+	int h1, h2, m1, m3;
 
+	for (h1 = 48; h1 < 51; h1++)
+	{
+		for (h2 = 48; h2 < 52; h2++)
+		{
+			for (m1 = 48; m1 < 54; m1++)
+			{
+				for (m2 = 48; m2 <= 57; m2++)
+				{
+					_printchar(h1);
+					_printchar(h2);
+					_printchar(':');
+					_printchar(m1);
+					_printchar(m2);
+					_printchar('\n');
+				}
+			}
+		}
+	}
 }
+
