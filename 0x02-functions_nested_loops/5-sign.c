6f530e9d9ddcaf814a1a0303539bc281f4249378
McWebmo <mcwebmo@gmail.com>
2023-08-15 07:09:47 -0700

    add 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - to check if a is +ve, -ve or zero
+ *
+ * Description: checks it using logical operators
+ * @n: a number
+ *
+ *
+ * Return: returns 1 if +ve, returns 0 if n is zero, returns -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (c > 0)
+		return (1);
+	else if (c < 0)
+		return (-1);
+	else
+		return (0);
+
+
+}
