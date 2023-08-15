a23cb1e29bb06d62447908819cb76434461d4ee3
McWebmo <mcwebmo@gmail.com>
2023-08-15 02:50:32 -0700

    add 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char phrase[] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(phrase[i]);
+	}
+
+	return (0);
+}
