f24a23a7f8d64e5fab462e8e56c199fcbe28aa14
McWebmo <mcwebmo@gmail.com>
2023-08-15 15:20:30 -0700

    if to while

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,22 +12,22 @@ void jack_bauer(void)
 {
 	int h1, h2, m1, m2;
 
-	for (h1 = 48; h1 < 51; h1++)
+	while (h1 != 50 && h2 != 51 && m1 != 53 && m2 != 57)
 	{
-		for (h2 = 48; h2 <= 57; h2++)
+		for (h1 = 48; h1 < 51; h1++)
 		{
-			for (m1 = 48; m1 < 54; m1++)
+			for (h2 = 48; h2 <= 57; h2++)
 			{
-				for (m2 = 48; m2 <= 57; m2++)
+				for (m1 = 48; m1 < 54; m1++)
 				{
-					if (h1 <= 50 && m1 <= 53 && m2 <= 57)
+					for (m2 = 48; m2 <= 57; m2++)
 					{
-						_putchar(h1);
-						_putchar(h2);
-						_putchar(58);
-						_putchar(m1);
-						_putchar(m2);
-						_putchar('\n');
+							_putchar(h1);
+							_putchar(h2);
+							_putchar(58);
+							_putchar(m1);
+							_putchar(m2);
+							_putchar('\n');
 					}
 				}
 			}
