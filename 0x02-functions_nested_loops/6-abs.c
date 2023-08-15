fabf31754761f1b576895a9bdb94dbb64b9e6cb6
McWebmo <mcwebmo@gmail.com>
2023-08-15 07:25:17 -0700

    add 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ *
+ * Description: finds absoulute value of n
+ * @n: an integer
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = n + (n * 2);
+
+	return (n);
+}
