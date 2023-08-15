576c1885b4721bfc2d4f7c1ef67967c23f23777a
McWebmo <mcwebmo@gmail.com>
2023-08-15 16:01:27 -0700

    fix 9 styling

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,28 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 			if (a * b < 10)
+			{
+				if (b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar((a * b) + '0');
+			}
 			else
 			{
+				
+				if (b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 				_putchar(((a * b) / 10) + '0');
 				_putchar(((a * b) % 10) + '0');
 			}
 
-			if (b != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			
 		}
 		_putchar('\n');
 	}
