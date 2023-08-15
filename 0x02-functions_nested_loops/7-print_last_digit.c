4d02016bffa9ffe15b68b6964b433bd98ec5912d
McWebmo <mcwebmo@gmail.com>
2023-08-15 07:41:30 -0700

    add 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ *
+ * Description: by getting the number mod 10
+ * @n: integer n
+ *
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
