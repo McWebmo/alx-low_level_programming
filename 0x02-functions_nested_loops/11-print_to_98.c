41c41d1b50de91a95085832c47436918ccdac8a5
McWebmo <mcwebmo@gmail.com>
2023-08-15 11:15:25 -0700

    betty 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,30 +13,30 @@
 
 void print_to_98(int n)
 {
-  if (n < 98)
-  {
-    for (; n < 99; n++)
-    {
-     printf("%d", n);
-     if (n != 98)
-      {
-       printf(", ");
-      }
-    }
+	if (n < 98)
+	{
+		for (; n < 99; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
 		printf("\n");
-  }
-  else if (n > 98)
-  {
-    for (; n > 97; n--)
-    {
-     printf("%d", n);
-     if (n != 98)
-      {
-       printf(", ");
-      }
-    }
+	}
+	else if (n > 98)
+	{
+		for (; n > 97; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
 		printf("\n");
-  }
-  else
-    printf("%d\n", 98);
+	}
+	else
+		printf("%d\n", 98);
 }
