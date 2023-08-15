a7507b34b3b5af3a2e2a4b3965678ba5e9feb869
McWebmo <mcwebmo@gmail.com>
2023-08-15 15:42:24 -0700

    add 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			_putchar((a * b) + '0');
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar('$');
+		}
+		_putchar("\n");
+	}
+
+}
