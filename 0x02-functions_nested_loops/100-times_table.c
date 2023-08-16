56aabc806e5ab3770fda0dcbda0bf8d384f2773b
McWebmo <mcwebmo@gmail.com>
2023-08-16 02:28:13 -0700

    fix 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - prints the times tables from 0 to 9
+ * print_times_table - prints the times tables from 0 to n
  *
  * Description: prints it without using stdio.h library
  *
