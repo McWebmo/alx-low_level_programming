3bf4f541c685bbbc8b4da3f01c260e10001b14c9
McWebmo <mcwebmo@gmail.com>
2023-08-16 02:57:12 -0700

    fix 48 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -47,7 +47,7 @@ void print_times_table(int n)
 						_putchar(',');
 						_putchar(' ');
 					}
-					_putchar((((a * b) / 100) % 10) + '0');
+					_putchar((((a * b) / 100) % 10) + 48);
 					_putchar((((a * b) / 10) % 10) + '0');
 					_putchar(((a * b) % 10) + '0');
 				}
