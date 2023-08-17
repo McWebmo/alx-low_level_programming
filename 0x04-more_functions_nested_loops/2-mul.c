f297a6eec1064298496224c712349dd241d80e6e
McWebmo <mcwebmo@gmail.com>
2023-08-17 09:50:39 -0700

    add 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description: multiplies 2 integers
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
