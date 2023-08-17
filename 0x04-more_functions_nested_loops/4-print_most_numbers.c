456873e6947a1f8c1265da84d1490cfd9c2798f6
McWebmo <mcwebmo@gmail.com>
2023-08-17 09:59:31 -0700

    add 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description: prints numbers from 0 to 9 except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+
+	int i = 0;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+			break;
+
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
