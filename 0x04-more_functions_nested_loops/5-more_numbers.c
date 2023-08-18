47786ab744b31bad640a72cb22b70bf002f7fbee
McWebmo <mcwebmo@gmail.com>
2023-08-18 05:15:51 -0700

    add 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,14 @@ int main(void)
 
 	for (i = 0; i <= 14; i++)
 	{
-		if 
+		if (i > 9)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		else
+			_putchar(i + '0');
+	
 	}
 
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ *
+ * Description: prints charchter _ n times
+ *
+ * @n: number of times _ is printed
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i <= 0)
+			break;
+
+		_putchar('_');
+	}
+
+	_putchar('\n');
+
+}
