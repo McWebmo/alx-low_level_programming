e18ebbe2cbe799f08cdb713e4e32bd73021a8384
McWebmo <mcwebmo@gmail.com>
2023-08-21 19:32:28 -0700

    fix 6 v7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,16 +13,14 @@
 void puts2(char *str)
 {
 	int c;
-	int i;
 
 	c = 0;
-	i = 0;
 
 	while (*(str + c) != 0)
 	{
 		_putchar(*(str + c));
 		c = c + 2;
-		i = c - 1;
+		int i = c - 1;
 	}
 	_putchar('\n');
 }
