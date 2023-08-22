aa044bbc401792eaed5715cd70636b242195e49c
McWebmo <mcwebmo@gmail.com>
2023-08-21 18:45:11 -0700

    add 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+void _put(char *str)
+{
+	int c;
+
+	c = 0;
+
+	while (*(str + c) != 0)
+	{
+		_putchar(*(str + c));
+		c++;
+	}
+}
