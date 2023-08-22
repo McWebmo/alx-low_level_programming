e899c40dabc9bd1ae0061ca39b0e6a14a3f64561
McWebmo <mcwebmo@gmail.com>
2023-08-21 18:49:53 -0700

    fix 3 v2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * _puts - Entry point
  *
- * Description:
+ * Description: prints a string to stdout
  *
- * Return: 0
+ * @str: any string
+ *
+ * Return: void
  */
 
 void _puts(char *str)
@@ -17,6 +19,7 @@ void _puts(char *str)
 	while (*(str + c) != 0)
 	{
 		_putchar(*(str + c));
+		_putchar('\n');
 		c++;
 	}
 }
