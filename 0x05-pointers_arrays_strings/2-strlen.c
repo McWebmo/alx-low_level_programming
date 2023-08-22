25cfe16f4837df3f586d0a36449fbdcb75f8ea73
McWebmo <mcwebmo@gmail.com>
2023-08-21 18:16:36 -0700

    add 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,41 @@
+/**
+ * _strlen - Entry point
+ *
+ * Description: returns length of a string
+ *
+ * @s: a string
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+
+	int counter = 0;
+	int len = 0;
+
+	while (*(s + counter) != "")
+	{
+		len++;
+		counter++;
+	}
+
+	return (len + 1);
+
+}
+
+/*
+
+ int main(void){
+
+	char *str;
+  int len;
+
+	str = "My first strleny";
+
+	printf("%c\n", *(str + 16));
+	return (0);
+
+}
+
+*/
