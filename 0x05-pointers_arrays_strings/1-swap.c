b55366e7c3c52aad4f1bd824d4a400ab70b68a84
McWebmo <mcwebmo@gmail.com>
2023-08-21 17:57:04 -0700

    add 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+
+
+/**
+ * swap_int - Entry point
+ *
+ * Description: swaps 2 integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	*a, *b = *b, *a;
+
+}
