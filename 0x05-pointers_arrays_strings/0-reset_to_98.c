4b9afb7878c299025fbcc941c0d45bc1d26279fa
McWebmo <mcwebmo@gmail.com>
2023-08-21 17:53:49 -0700

    init and add 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: takes a pointer as input and changes its value to 98
+ *
+ * @n: a pointer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
