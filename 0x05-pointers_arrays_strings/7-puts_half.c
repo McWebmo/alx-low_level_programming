2651078fd77ba87fa0e35e06e76738752e90090a
McWebmo <mcwebmo@gmail.com>
2023-08-23 21:51:31 -0700

    7.1.1

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+
+	int len;
+	int i;
+
+	for (len = 0; str[len] != 0; ++len)
+		;
+
+	if (len % 2 != 0)
+	{
+		i = (len - 1) / 2;
+
+		for (i = 0; str[i] != 0; i++)
+			_putchar(str[i]);
+	}
+
+	else if (len % 2 = 0)
+	{
+		i = len / 2;
+
+		for (i = 0; str[i] != 0; i++)
+			_putchar(str[i]);
+	}
+
+
+
+}
