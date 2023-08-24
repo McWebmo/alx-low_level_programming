85dcf8f68feaecc393d17b9640c6186b37e0a381
McWebmo <mcwebmo@gmail.com>
2023-08-23 22:31:12 -0700

    8.6

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -19,7 +19,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+		if (a[i] == a[n])
+			printf("\n");
+		else
+			printf(", ");
 	}
 
 }
