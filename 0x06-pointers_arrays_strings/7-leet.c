2862e1dd5f39d9b6dbad1d6e12290741cab30af5
McWebmo <mcwebmo@gmail.com>
2023-08-25 19:49:24 -0700

    7.1

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+char *leet(char *s)
+{
+
+	char abc[10] = {a, A, e, E, o, O, t, T, l, L};
+	int leet[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		for (l = 0; s[i] != abc[l]; l++)
+			continue;
+		if (s[i] == abc[l])
+			s[i] == leet[l];
+	}
+
+	return (s);
+
+}
