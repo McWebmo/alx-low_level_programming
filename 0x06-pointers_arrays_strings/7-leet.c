d46a72c11de590ddf89d2a32e6d84a1fdc9716ed
McWebmo <mcwebmo@gmail.com>
2023-08-25 19:51:29 -0700

    7.3

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,6 +14,7 @@ char *leet(char *s)
 	char abc[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int leet[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 	int i;
+	int l;
 
 	for (i = 0; s[i] != 0; i++)
 	{
