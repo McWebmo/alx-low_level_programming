26f5699e75704ab89773e52b1de4fb0678b4f47b
McWebmo <mcwebmo@gmail.com>
2023-08-29 23:17:16 -0700

    add 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+
+	int i = 0;
+
+	if (s[i] == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(s[i]);
+	_putchar(_put_recursion(s[i + 1]));
+
+}
