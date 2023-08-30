90174ca83fdd28bb353aa19dd481ce967c822326
McWebmo <mcwebmo@gmail.com>
2023-08-30 02:11:28 -0700

    4.0

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y-1));
+
+}
