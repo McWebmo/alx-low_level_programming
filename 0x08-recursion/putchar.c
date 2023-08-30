a8fd1c350b27a5124ce36a9d944e1ee023b1ea3e
McWebmo <mcwebmo@gmail.com>
2023-08-29 23:12:14 -0700

    init

---
+++ 0x08-recursion/putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char phrase[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(phrase[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
