15481860aeeb00f85a79876f244f3ab5bad94a55
McWebmo <mcwebmo@gmail.com>
2023-08-30 02:15:37 -0700

    4.1

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,14 @@
 
 
 /**
- * main - Entry point
+ *  _pow_recursion - Entry point
  *
- * Description:
+ * Description: a function that returns the value of x raised to the power of y
  *
- * Return: 0
+ * @x: number to be raised
+ * @y: power
+ *
+ * Return: the result, -1 indicates error
  */
 
 int _pow_recursion(int x, int y)
@@ -17,6 +20,6 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 		return (1);
 
-	return (x * _pow_recursion(x, y-1));
+	return (x * _pow_recursion(x, y - 1));
 
 }
