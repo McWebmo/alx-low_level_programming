2d73e19c773673534ea06f84abf5680261961a54
McWebmo <mcwebmo@gmail.com>
2023-08-30 01:57:31 -0700

    2.1

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	int i = 0;
+	int len = 0;
+
+	if (s[i] == 0)
+		return (0);
+
+	return (len + _strlen_recursion(s + 1));
+
+}
