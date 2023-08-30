55d1cddc3bbe405dbd1f73fa6f86215094e459ba
McWebmo <mcwebmo@gmail.com>
2023-08-30 12:09:00 -0700

    5.6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,17 @@
 
 
 /**
- * main - Entry point
+ * secondary - Entry point
  *
- * Description:
+ * Description: supporting fucntion for the main secondarytion
  *
- * Return: 0
+ * @i: i is the numbers being sqaured
+ * @n: n is the number we would like to find a sqrt for
+ *
+ * Return: returns i
  */
 
-int func(int i, int n)
+int secondary(int i, int n)
 {
 	if (i * i == n)
 		return (i);
@@ -18,14 +21,24 @@ int func(int i, int n)
 		return (-1);
 
 
-	return(func(i, n));
+	return (secondary(i, n));
 }
+
+/**
+ * _sqrt_recursion - Entry point
+ *
+ * Description: finds square root of a number n
+ *
+ * @n: number to find the square root of
+ *
+ * Return: returns the square root or -1 if there is no natural square root
+ */
 
 int _sqrt_recursion(int n)
 {
 
 	int i = 0;
 
-	return (func(i, n));
+	return (secondary(i, n));
 
 }
