9528a2b1392bfc236155b0a5e405506c46cd3ae5
McWebmo <mcwebmo@gmail.com>
2023-09-01 17:24:32 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints number of arguments
+ *
+ * @argc: number of arguments
+ * @argv: array of strings of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+
+}
