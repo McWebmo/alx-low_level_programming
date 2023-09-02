8efefd40f8812c5466320784cb51ff8073871fe6
McWebmo <mcwebmo@gmail.com>
2023-09-01 17:07:40 -0700

    0.1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the files name
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	for (i = 0; i < argc; i++)
+		_putchar(argv[i]);
+
+	return (0);
+
+}
