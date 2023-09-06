503ca4efaaa28b2a36c58f6d0d8ebb8b0b55c934
McWebmo <mcwebmo@gmail.com>
2023-09-06 04:51:46 -0700

    0.1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char array[];
+
+	if (size == 0) {
+		return (null);
+	}
+
+	array = malloc(size);
+
+	array[0] = c;
+
+	return (*array);
+
+}
