a2bea08cd0fcef5cfc27cdd5190fb6923c1e7c3b
McWebmo <mcwebmo@gmail.com>
2023-09-11 16:41:18 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,20 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog {
+/**
+ * struct dog - a struct called dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
 
 	char *name;
 	float age;
 	char *owner;
-	
 };
-
-#endif // !DEBUG
+#endif
