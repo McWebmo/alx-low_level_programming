2f42380dc4de142bc8d20e2a747ba864da64863a
McWebmo <mcwebmo@gmail.com>
2023-09-12 01:24:50 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,20 +15,17 @@ void print_dog(struct dog *d)
 
 	if (d != NULL)
 	{
-		if (d.name == NULL)
+		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", (*d).name);
 
-		if (d.age == NULL)
-			printf("age: (nil)\n");
-		else
-			printf("age: %d\n", d.age);
+		printf("Age: %f\n", (*d).age);
 
-		if (d.owner == NULL)
-			printf("owner: (nil)\n");
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
 		else
-			printf("owner: %s\n", (*d).owner);
+			printf("Owner: %s\n", (*d).owner);
 	}
 
 
